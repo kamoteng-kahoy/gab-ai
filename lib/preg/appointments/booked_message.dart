50a22f34ad78df55cba1e381e8e5635d94ca3d02
James Autor <jautor_210000001078@uic.edu.ph>
2024-10-03 14:26:56 +0800

    Remove video_thumbnail dependency from pubspec.yaml and reset the booked_messages.dart file

--- lib/preg/appointments/booked_message.dart
+++ lib/preg/appointments/booked_message.dart
@@ -1,97 +1,12 @@
 import 'package:fluentui_system_icons/fluentui_system_icons.dart';
-import 'dart:io'; // Import the dart:io package
 import 'package:flutter/material.dart';
 import 'package:gab_ai/colors.dart';
-import 'package:file_picker/file_picker.dart';
-import 'package:gab_ai/preg/appointments/picture_preview.dart';
-import 'package:video_player/video_player.dart'; // Import the video_player package
 
-class BookedMessage extends StatefulWidget {
+class BookedMessage extends StatelessWidget {
   final String name;
   final String profilePicture;
 
   const BookedMessage({super.key, required this.name, required this.profilePicture});
-
-  @override
-  _BookedMessageState createState() => _BookedMessageState();
-}
-
-class Message {
-  final String type;
-  final String content;
-  final String? fileType;
-
-  Message({required this.type, required this.content, this.fileType});
-}
-
-class _BookedMessageState extends State<BookedMessage> {
-  final TextEditingController _controller = TextEditingController();
-  final FocusNode _focusNode = FocusNode();
-  final List<Message> messages = [];
-  final ScrollController _scrollController = ScrollController();
-  String? _filePath;
-  String? _fileType;
-  
-  @override
-  void initState() {
-    super.initState();
-    _focusNode.addListener(_scrollToBottom);
-  }
-
-  @override
-  void dispose() {
-    _focusNode.removeListener(_scrollToBottom);
-    _scrollController.dispose();
-    _focusNode.dispose();
-    _controller.dispose();
-    super.dispose();
-  }
-
-  void _scrollToBottom() {
-    if (_focusNode.hasFocus) {
-      Future.delayed(const Duration(milliseconds: 100), () {
-        _scrollController.animateTo(
-          _scrollController.position.maxScrollExtent,
-          duration: const Duration(milliseconds: 300),
-          curve: Curves.easeOut,
-        );
-      });
-    }
-  }
-
-  void _sendMessage() {
-    if (_controller.text.isNotEmpty) {
-      setState(() {
-        if (_filePath != null && _fileType != null) {
-          messages.add(Message(type: 'file', content: _filePath!, fileType: _fileType));
-          _filePath = null;
-          _fileType = null;
-        } else {
-          messages.add(Message(type: 'text', content: _controller.text));
-        }
-        _controller.clear();
-      });
-      _scrollToBottom();
-      _focusNode.requestFocus(); // Retain the keyboard
-    }
-  }
-
-  void _uploadFiles() async {
-    FilePickerResult? result = await FilePicker.platform.pickFiles(
-      type: FileType.custom,
-      allowedExtensions: ['jpg', 'png', 'mp4'],
-    );
-
-    if (result != null) {
-      PlatformFile file = result.files.first;
-      setState(() {
-        _controller.text = file.name; // Show file name in the text box
-        _filePath = file.path;
-        _fileType = file.extension;
-      });
-      print(file.name);
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -99,298 +14,153 @@ class _BookedMessageState extends State<BookedMessage> {
       backgroundColor: SystemColors.bgColorLighter,
       appBar: AppBar(
         backgroundColor: SystemColors.bgColorLighter,
-        toolbarHeight: 70,
         title: Row(
           children: [
             CircleAvatar(
-              backgroundImage: NetworkImage(widget.profilePicture),
+              backgroundImage: NetworkImage(profilePicture),
             ),
-            const SizedBox(width: 15),
-            Text(widget.name),
+            const SizedBox(width: 10),
+            Text(name),
           ],
         ),
+        toolbarHeight: 80,
         leading: IconButton(
-          icon: const Icon(FluentIcons.arrow_left_20_filled), // Change this to any icon you prefer
-          onPressed: () {
-            Navigator.of(context).pop();
-          },
+          icon: const Icon(FluentIcons.arrow_left_24_regular),
+          onPressed: () => Navigator.pop(context),
         ),
       ),
-      body: Column(
-        children: [
-          Expanded(
-            child: SingleChildScrollView(
-              controller: _scrollController,
-              child: MessageList(messages: messages),
-            ),
-          ),
-          MessageInput(
-            controller: _controller,
-            focusNode: _focusNode,
-            onSendMessage: _sendMessage,
-            onUploadFiles: _uploadFiles,
-          ),
-        ],
-      ),
+      body: const ChatScreen(),
     );
   }
 }
 
-class ChatBubble extends StatelessWidget {
-  final String message;
-
-  const ChatBubble({super.key, required this.message});
+class ChatScreen extends StatefulWidget {
+  const ChatScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    // Get the keyboard height
-    final keyboardHeight = MediaQuery.of(context).viewInsets.bottom;
+  _ChatScreenState createState() => _ChatScreenState();
+}
 
-    return SingleChildScrollView(
-      reverse: true,
-      padding: EdgeInsets.only(bottom: keyboardHeight),
+class _ChatScreenState extends State<ChatScreen> {
+  final List<ChatMessage> _messages = [];
+  final TextEditingController _controller = TextEditingController();
+
+  void _handleSubmitted(String text) {
+    _controller.clear();
+    ChatMessage message = ChatMessage(
+      text: text,
+      isSent: true,
+    );
+    setState(() {
+      _messages.insert(0, message);
+    });
+  }
+
+  Widget _buildTextComposer() {
+    return IconTheme(
+      data: IconThemeData(color: Theme.of(context).primaryColor),
       child: Container(
-        padding: const EdgeInsets.all(10),
-        margin: const EdgeInsets.symmetric(vertical: 5, horizontal: 12),
-        constraints: BoxConstraints(
-          maxWidth: MediaQuery.of(context).size.width * 0.75,
-        ),
-        decoration: BoxDecoration(
-          color: SystemColors.primaryColorDarker,
-          borderRadius: BorderRadius.circular(15),
-        ),
-        child: Text(
-          message,
-          style: const TextStyle(color: Colors.white),
-          textAlign: TextAlign.left,
-        ),
-      ),
-    );
-  }
-}
-
-class MessageList extends StatelessWidget {
-  final List<Message> messages;
-
-  const MessageList({super.key, required this.messages});
-
-  @override
-  Widget build(BuildContext context) {
-    return ListView.builder(
-      shrinkWrap: true,
-      physics: const NeverScrollableScrollPhysics(),
-      itemCount: messages.length,
-      itemBuilder: (context, index) {
-        final message = messages[index];
-        if (message.type == 'text') {
-          return Row(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              Flexible(
-                child: ChatBubble(message: message.content)),
-            ],
-          );
-        } else if (message.type == 'file') {
-          return Row(
-            mainAxisAlignment: MainAxisAlignment.end, // Align the file to the right
-            children: [
-              Container(
-                margin: const EdgeInsets.symmetric(vertical: 5.0, horizontal: 10.0),
-                padding: const EdgeInsets.all(10.0),
-                decoration: BoxDecoration(
-                  color: SystemColors.primaryColorDarker,
-                  borderRadius: BorderRadius.circular(10.0),
-                ),
-                child: GestureDetector(
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => ImagePreviewScreen(imagePath: message.content),
-                      ),
-                    );
-                  },
-                  child: _buildFileWidget(message.content),
-                ),
-              ),
-            ],
-          );
-        }
-        return const SizedBox.shrink();
-      },
-    );
-  }
-
-  Widget _buildFileWidget(String filePath) {
-    final file = File(filePath);
-    final fileExtension = file.path.split('.').last.toLowerCase();
-
-    if (['jpg', 'jpeg', 'png', 'gif', 'mp4', 'mov'].contains(fileExtension)) {
-      return FutureBuilder<File>(
-        future: Future.delayed(const Duration(seconds: 2), () async {
-          if (await file.exists()) {
-            return file;
-          } else {
-            throw Exception('File does not exist');
-          }
-        }),
-        builder: (BuildContext context, AsyncSnapshot<File> snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return Center(
-              child: SizedBox(
-                width: 200,
-                height: 200,
-                child: Stack(
-                  alignment: Alignment.center,
-                  children: [
-                    Container(
-                      width: 200,
-                      height: 200,
-                      decoration: BoxDecoration(
-                        color: Colors.grey[300],
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      child: const SizedBox(
-                        height: 24,
-                        width: 24,
-                        child: CircularProgressIndicator(),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            );
-          } else if (snapshot.hasError) {
-            return Center(
-              child: Text('Error loading file: ${snapshot.error}'),
-            );
-          } else if (snapshot.hasData) {
-            try {
-              return _displayFile(snapshot.data!);
-            } catch (e) {
-              return const Center(
-                child: Text('Invalid file data'),
-              );
-            }
-          } else {
-            return const Center(
-              child: Text('File not found'),
-            );
-          }
-        },
-      );
-    } else {
-      return const Center(
-        child: Text('Unsupported file type'),
-      );
-    }
-  }
-
-  Widget _displayFile(File file) {
-    final fileExtension = file.path.split('.').last.toLowerCase();
-
-    if (['jpg', 'jpeg', 'png', 'gif'].contains(fileExtension)) {
-      return Image.file(
-        file,
-        width: 200, // Set the desired width
-        height: 200, // Set the desired height
-        fit: BoxFit.cover, // Adjust the image to cover the box
-      );
-    } else if (['mp4', 'mov'].contains(fileExtension)) {
-      return _displayVideo(file);
-    } else {
-      throw Exception('Unsupported file type');
-    }
-  }
-
-  Widget _displayVideo(File file) {
-    VideoPlayerController controller = VideoPlayerController.file(file);
-
-    return FutureBuilder<void>(
-      future: controller.initialize(),
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.done) {
-          return Stack(
-            alignment: Alignment.center,
-            children: [
-              SizedBox(
-                width: 200, // Set the desired width
-                height: 200, // Set the desired height
-                child: AspectRatio(
-                  aspectRatio: controller.value.aspectRatio,
-                  child: VideoPlayer(controller),
-                ),
-              ),
-              Icon(
-                Icons.play_circle_outline,
-                size: 50,
-                color: Colors.white.withOpacity(0.7),
-              ),
-            ],
-          );
-        } else {
-          return const Center(child: CircularProgressIndicator());
-        }
-      },
-    );
-  }
-}
-
-class MessageInput extends StatelessWidget {
-  final TextEditingController controller;
-  final FocusNode focusNode;
-  final VoidCallback onSendMessage;
-  final VoidCallback onUploadFiles;
-
-  const MessageInput({
-    super.key,
-    required this.controller,
-    required this.focusNode,
-    required this.onSendMessage,
-    required this.onUploadFiles,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.only(left: 10),
-      margin: const EdgeInsets.symmetric(vertical: 10),
-      child: Row(
-        children: [
-          Expanded(
-            child: SizedBox(
-              child: SingleChildScrollView(
-                scrollDirection: Axis.vertical,
+        color: SystemColors.bgColorLighter,
+        margin: const EdgeInsets.symmetric(horizontal: 8.0),
+        child: Row(
+          children: <Widget>[
+            Flexible(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 8.0),
                 child: TextField(
-                  controller: controller,
-                  focusNode: focusNode,
+                  controller: _controller,
+                  onSubmitted: _handleSubmitted,
+                  maxLines: 7, // Allow the text field to grow vertically
+                  minLines: 1, // Minimum number of lines
                   decoration: InputDecoration(
-                    hintText: 'Type a message...',
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(15),
+                    hintText: 'Send a message',
+                    hintStyle: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                      color: SystemColors.textColorDarker,
+                      fontSize: 16,
                     ),
-                    contentPadding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15.0),
+                    ), // Add border
                   ),
                   style: Theme.of(context).textTheme.bodyMedium?.copyWith(
-                    fontWeight: FontWeight.w500,
+                    color: SystemColors.textColorDarker,
                     fontSize: 16,
                   ),
-                  onSubmitted: (value) => onSendMessage(),
-                  minLines: 1,
-                  maxLines: 5,
+                  scrollPadding: const EdgeInsets.all(20.0), // Add scroll padding
                 ),
               ),
             ),
+            IconButton(
+              icon: const Icon(FluentIcons.attach_24_filled,
+                color: SystemColors.textColorDarker,
+              ),
+              onPressed: () => print('Attach'),
+            ),
+            IconButton(
+              icon: const Icon(FluentIcons.send_24_filled,
+                color: SystemColors.textColorDarker,
+              ),
+              onPressed: () => _handleSubmitted(_controller.text),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Flexible(
+          child: ListView.builder(
+            padding: const EdgeInsets.all(8.0),
+            reverse: true,
+            itemBuilder: (_, int index) => _messages[index],
+            itemCount: _messages.length,
           ),
-          IconButton(
-            icon: const Icon(FluentIcons.attach_20_filled),
-            onPressed: onUploadFiles,
+        ),
+        const Divider(height: 1.0),
+        Container(
+          decoration: BoxDecoration(
+            color: Theme.of(context).cardColor,
           ),
-          IconButton(
-            icon: const Icon(FluentIcons.send_20_filled),
-            onPressed: onSendMessage,
+          child: _buildTextComposer(),
+        ),
+      ],
+    );
+  }
+}
+
+class ChatMessage extends StatelessWidget {
+  final String text;
+  final bool isSent;
+
+  const ChatMessage({super.key, required this.text, required this.isSent});
+
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: isSent ? Alignment.centerRight : Alignment.centerLeft,
+      child: Container(
+        padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 14),
+        margin: const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+        decoration: BoxDecoration(
+          color: isSent ? SystemColors.primaryColorDarker : Colors.grey[300],
+          borderRadius: BorderRadius.only(
+            topLeft: const Radius.circular(15),
+            topRight: const Radius.circular(15),
+            bottomLeft: isSent ? const Radius.circular(15) : const Radius.circular(0),
+            bottomRight: isSent ? const Radius.circular(0) : const Radius.circular(15),
           ),
-        ],
+        ),
+        child: Text(
+          text,
+          style: TextStyle(
+            color: isSent ? Colors.white : Colors.black,
+            fontSize: 16,
+          ),
+        ),
       ),
     );
   }
